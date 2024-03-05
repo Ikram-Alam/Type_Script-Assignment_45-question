d7549c4fbeaff8ad69f29f219dcff37a768e277f
Ikram Alam <ikramalam515@gmail.com>
2024-03-05 12:14:07 +0500

    Second commit message

---
+++ 14/me.js
@@ -0,0 +1,5 @@
+var dinnerGuests = ["Ronaldo", "Babar Azam", "Cris Evan"];
+// Print invitation messages to each person
+for (var i = 0; i < dinnerGuests.length; i++) {
+    console.log("Dear ".concat(dinnerGuests[i], ",\nYou are cordially invited to dinner. We would be honored to have your presence."));
+}
---
+++ 14/me.ts
@@ -0,0 +1,6 @@
+let dinnerGuests: string[] = ["Ronaldo", "Babar Azam", "Cris Evan"];
+
+// Print invitation messages to each person
+for (let i = 0; i < dinnerGuests.length; i++) {
+    console.log(`Dear ${dinnerGuests[i]},\nYou are cordially invited to dinner. We would be honored to have your presence.`);
+}
